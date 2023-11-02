5b74457b645204969b9c8e9ee63e649d1f693544
hsanch13 <hayleysanchez13@gmail.com>
2023-11-02 16:12:24 -0600

    created functions

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if (someValue < 42) {
+        return 42 - someValue
+    }
+
+    else {
+        return someValue - 42
+    }
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264
+
+} 
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination < start) {
+    return ((start - destination) * 264)
+    }
+
+    else {
+        return ((destination - start) * 264)
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination)
+    if (distance <= 400) {
+        return 0
+    }
+
+    else if (400 <= distance && distance <= 2000) {
+        return 0.02 * (distance - 400)
+    }
+     
+    else if (2000 <= distance && distance <= 2500) {
+        return 25
+    }
+
+    else if (distance > 2500) {
+        return 'cannot travel that far'
+    }
+    
+    }
